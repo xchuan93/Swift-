3c1fae3901dadfb2d267ff1bd48fd6fd216558f3
XuWenYan <xuwenyan@WFYdeMacBook-Pro.local>
2019-01-02 16:08:33 +0800

    first commit

---
+++ SwiftTestOne/TestTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TestTableViewCell.swift
+//  SwiftTestOne
+//
+//  Created by XuWenYan on 2019/1/2.
+//  Copyright © 2019年 XChuan. All rights reserved.
+//
+
+import UIKit
+
+class TestTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
