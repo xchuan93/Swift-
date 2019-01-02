84800ce253715a3405113ba79d1a497668847783
XuWenYan <xuwenyan@wfydemacbook-pro.local>
2019-01-02 13:40:16 +0800

    Initial Commit

---
+++ SwiftTestOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTestOne
+//
+//  Created by XuWenYan on 2019/1/2.
+//  Copyright © 2019年 XChuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
